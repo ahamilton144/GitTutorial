1d402c92ae30c9375c0d179d162e9c311f346c01
ahamilton144 <ahamilton144@gmail.com>
2019-09-08 12:35:13 -0400

    add input data & main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('data/cleveland_fed_yieldcurve.csv', sep='\t')
+df.columns = ['Date','GDP_Growth','Spread_10Y_3M']
+df.index = pd.to_datetime(df.Date)
+del df['Date']
+
+fig = plt.figure()
+plt.plot(df)
+plt.xlabel('Date')
+plt.ylabel('Percentage points')
+plt.legend(df.columns)
+plt.show()
+plt.savefig('results/yieldcurve.png')
+
