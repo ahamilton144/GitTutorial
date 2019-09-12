eaf44fd07a15cc876b4ffde044788d746fb54d6b
jehill17@live.unc.edu <31226517+jehill17@users.noreply.github.com>
2019-09-12 15:19:55 -0400

    Joy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/cleveland_fed_yieldcurve.csv', sep='\t')
-df.columns = ['Date','GDP_Growth','Spread_10Y_3M']
+df.columns = ['Date','Growth','Spread']
 df.index = pd.to_datetime(df.Date)
 del df['Date']
 
@@ -12,5 +12,5 @@ plt.xlabel('Date')
 plt.ylabel('Percentage points')
 plt.legend(['GDP Growth', '10Y-3M Spread'])
 plt.show()
-plt.savefig('results/yieldcurve_GDP.png')
+plt.savefig('results/yieldcurve_GDP_JOY.png')
 
